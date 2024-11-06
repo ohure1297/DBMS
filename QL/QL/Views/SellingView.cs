9937389d4d7d5d5b06ba90fc4c4137b70b7d1984
Thang <thanghuynh0706@gmail.com>
2024-11-06 17:04:01 +0700

    Add

--- QL/QL/Views/SellingView.cs
+++ QL/QL/Views/SellingView.cs
@@ -32,7 +32,7 @@ namespace QL.Views
 
             foreach (DataRow row in productTable.Rows)
             {
-                MessageBox.Show(row["MaSPham"].ToString());
+                //MessageBox.Show(row["MaSPham"].ToString());
                 if (row["MaSPham"] != null)
                 {
                     UCProduct uCProduct = new UCProduct();
