81a0e065a9de402ff436d2a65e4c172937ce6f9c
Jucky901 <truongvuon235@gmail.com>
2024-11-08 09:26:03 +0700

    fix the connection string

--- QL/QL/DAO/ProductDAO.cs
+++ QL/QL/DAO/ProductDAO.cs
@@ -13,7 +13,7 @@ namespace QL.DAO
     public class ProductDAO
     {
         DBConnection db = new DBConnection();
-        String connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CuaHangTienLoi;Integrated Security=True;Encrypt=True";
+        String connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CuaHangTienLoi;Integrated Security=True;";
 
         public DataTable DataTable_Product()
         {
