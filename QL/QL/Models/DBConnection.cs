e036f8a6986787555acd54457bd626485bf72c80
Thang <thanghuynh0706@gmail.com>
2024-11-14 01:27:45 +0700

    DB

--- QL/QL/Models/DBConnection.cs
+++ QL/QL/Models/DBConnection.cs
@@ -5,18 +5,31 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace QL.Models
 {
     public class DBConnection
     {
+
         SqlConnection conn = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CuaHangTienLoi;Integrated Security=True");
+        //Set conn null để sau này sau khi đăng nhập mới sử dụng hàm để set conn lại sau theo chức vụ, tạm thời để như cũ nhằm để test
         public SqlConnection getConnection
         {
             get
             {
                 return conn;
             }
+        }
+
+        public void changeStrConnectToNhanVien()
+        {
+            conn = new SqlConnection("Data Source = (localdb)\\mssqllocaldb; Initial Catalog = CuaHangTienLoi; Persist Security Info = True; User ID = nhanvien; Password = nhanvien");
+        }
+
+        public void changeStrConnectToQuanLy()
+        {
+            conn = new SqlConnection("Data Source = (localdb)\\mssqllocaldb; Initial Catalog = CuaHangTienLoi; Persist Security Info = True; User ID = quanly; Password = quanly");
         }
 
         public void openConnection()
